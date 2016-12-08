c698122bad8bf66606743d369fd5ac7a76dc189b
Malikov <malikov.yuriy.alex@gmail.com>
2016-12-08 18:08:27 +0200

    some changes to main

--- src/main/java/ua/goit/javaee/group2/dao/ProjectDAO.java
+++ src/main/java/ua/goit/javaee/group2/dao/ProjectDAO.java
@@ -2,6 +2,27 @@ package ua.goit.javaee.group2.dao;
 
 import ua.goit.javaee.group2.model.Project;
 
-public interface ProjectDAO extends AbstractDAO<Project> {
+import java.sql.SQLException;
+import java.util.List;
 
+public interface ProjectDAO extends AbstractDAO<Project> {
+    @Override
+    Project save(Project object) throws SQLException{
+
+    };
+
+    @Override
+    boolean saveAll(List<Project> list) throws SQLException;
+
+    @Override
+    Project load(int id) throws SQLException;
+
+    @Override
+    List<Project> findAll() throws SQLException;
+
+    @Override
+    boolean deleteById(int id) throws SQLException;
+
+    @Override
+    boolean deleteAll() throws SQLException;
 }
